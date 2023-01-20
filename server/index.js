b60a1bba2eff48bd0573164a81806be91975df13
Jeremy <jdoski32@gmail.com>
2023-01-20 18:23:04 -0500

    added railway deployment

--- server/index.js
+++ server/index.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.send({hi: 'there'});
+    res.send({bye: 'there'});
 });
 
-app.listen(5000);
-
-app.get('test', (req, res) => {
-    res.send({test: 'testing'});
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
