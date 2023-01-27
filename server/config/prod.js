fdfafdeeb02459a244b79216acac358de575bba7
ScoobyDooski <31867383+ScoobyDooski@users.noreply.github.com>
2023-01-27 16:58:43 -0500

    Update prod.js

--- server/config/prod.js
+++ server/config/prod.js
@@ -4,10 +4,3 @@ module.exports = {
     mongoURI: process.env.MONGO_URI,
     cookieKey: process.env.COOKIE_KEY,
 };
-
-
-//test db password: p16AzwOpZkxxfQww
-//test db code: mongodb+srv://scoobydooski:p16AzwOpZkxxfQww@cluster0.gazgegv.mongodb.net/testdb?retryWrites=true&w=majority
-
-//googleclientidprod: 489910541774-vdl6ud14l1ouhh221qmig5742ig5tn9e.apps.googleusercontent.com
-//googleclientsecretprod: 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
