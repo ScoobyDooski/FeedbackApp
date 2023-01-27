6f4adafbf6e2b0ae4af4a9e82f7f63736865842c
ScoobyDooski <31867383+ScoobyDooski@users.noreply.github.com>
2023-01-27 16:58:30 -0500

    Update keys.js

--- server/config/keys.js
+++ server/config/keys.js
@@ -7,11 +7,3 @@ if (process.env.NODE_ENV === 'production'){
     //in dev return dev keys
     module.exports = require('./dev');
 }
-
-
-
-//test db password: p16AzwOpZkxxfQww
-//test db code: mongodb+srv://scoobydooski:p16AzwOpZkxxfQww@cluster0.gazgegv.mongodb.net/testdb?retryWrites=true&w=majority
-
-//googleclientidprod: 489910541774-vdl6ud14l1ouhh221qmig5742ig5tn9e.apps.googleusercontent.com
-//googleclientsecretprod: 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
