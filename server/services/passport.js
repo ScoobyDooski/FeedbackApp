4299e55a1ffeb09d7a3e73ba0bad9f71bec12ec1
Jeremy <jdoski32@gmail.com>
2023-02-02 10:32:43 -0500

    Start of client integration

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,42 +1,40 @@
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('../config/keys');
-const mongoose = require('mongoose');
-
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("../config/keys");
+const mongoose = require("mongoose");
 
 //The mongoose model Class that creates the collection for us to inherit from
-const User = mongoose.model('users');
+const User = mongoose.model("users");
 
 //user = user mongoose model turned into id to place in cookie
 passport.serializeUser((user, done) => {
-    //user.id is the database id, every user is guarnteed to have this id, not every user may have a google id
-    done(null, user.id);
+  //user.id is the database id, every user is guarnteed to have this id, not every user may have a google id
+  done(null, user.id);
 });
 
-
 passport.deserializeUser((id, done) => {
-  User.findById(id).then(user => {
-    done(null,user);
+  User.findById(id).then((user) => {
+    done(null, user);
   });
 });
 
 //creating a google strategy to be used by passport JS in the authentication. This is the setup before authenticating users.
 passport.use(
-    new GoogleStrategy({
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback',
-        proxy: true
-    }, 
-    (accessToken, refreshToken, profile, done) => {
-        User.findOne({ googleId: profile.id}).then((existingUser) => {
-            if(existingUser) {
-                done(null, existingUser);
-            } else {
-                new User({googleId: profile.id})
-                    .save()
-                    .then(user => done(null, user));
-            }
-        });
-    })
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+      proxy: true,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
+    }
+  )
 );
